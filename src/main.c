47c81ef8bb461804f160ffdc8c798ad3bf13b333
emilyhuang <emily.hby@gmail.com>
2020-03-27 23:00:52 -0400

    Have walls from top now. Need to finish walls from other directions.

--- src/main.c
+++ src/main.c
@@ -7,9 +7,86 @@ int sideLength = 24;
 volatile int pixel_buffer_start = 0xC8000000; // global variable
 short int colour[4] = {0xF800, 0x07E0, 0x001F, 0xAAAA};
 //direction: 0:left 1:top 2:right 3:bottom 
+short int backgroundColour = 0x0000;
+int wall[8] = {-1, -1, -1, -1, -1, -1, -1, -1};//-1 if not taken
+int middleOpening[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
+int dir[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
+int wallColour[8] = {-1, -1, -1, -1, -1, -1, -1, -1};
 
+void wallFromLeft(int colToColour,int middleOpening, short int wallColour);
+void wallFromTop(int rowToColour,int middleOpening, short int wallColour);
+void wallFromRight(int colToColour,int middleOpening, short int wallColour);
+void wallFromBottom(int rowToColour,int middleOpening, short int wallColour);
+void clear_screen();
+void wait_for_vsync();
+void plot_pixel(int x, int y, short int line_color);
+void draw_grid(int a, int b, short int gridColour);
+void draw_background();
 
-// code for subroutines (not shown)
+int main(void){
+    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+    // declare other variables(not shown)
+    // initialize location and direction of rectangles(not shown)
+
+    /* set front pixel buffer to start of FPGA On-chip memory */
+    *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the 
+                                        // back buffer
+    /* now, swap the front/back buffers, to set the front buffer location */
+    wait_for_vsync();
+    /* initialize a pointer to the pixel buffer, used by drawing functions */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen();
+    draw_background(); // pixel_buffer_start points to the pixel buffer
+    /* set back pixel buffer to start of SDRAM memory */
+    *(pixel_ctrl_ptr + 1) = 0xC0000000;
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+
+    for(int i = 0; i < 8; i++){
+        middleOpening[i] = rand()%8+1;
+        wallColour[i] = colour[rand()%3];
+        dir[i] = rand()%4;
+    }
+
+    while (1){   
+        wait_for_vsync(); // swap front and back buffers on VGA vertical sync
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+        draw_background();
+        randomBars();
+    }
+}
+
+void randomBars(){
+    clear_screen();
+    draw_background();
+    //generate a bar with a random opening, random colour, coming from a random direction
+    
+    //find unoccupied wall element: take 1st one for now
+    wallFromTop(wall[0], middleOpening[0], wallColour[0]);
+    // switch(dir){
+    //     case 0:
+    //         wallFromLeft(middleOpening, wallColour);
+    //         break;
+    //     case 1:
+    //         wallFromTop(middleOpening, wallColour);
+    //         break;
+    //     case 2:
+    //         wallFromRight(middleOpening, wallColour);
+    //         break;
+    //     case 3:
+    //         wallFromBottom(middleOpening, wallColour);
+    //         break; 
+    // }
+    wall[0] += 1;
+}
+
+void clear_screen(){
+    for(int j = 0; j < 240; j++){
+        for(int i = 0; i < 320; i++){
+            plot_pixel(i, j, backgroundColour);
+        }
+    }
+}
+
 void wait_for_vsync(){
     volatile int * pixel_ctrl_ptr = (int *) 0xFF203020;//beginning addr of DMA controller
     register int status;
@@ -26,104 +103,67 @@ void plot_pixel(int x, int y, short int line_color)
     *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
 }
 
-void draw_grid(int a, int b){
+void draw_grid(int a, int b, short int gridColour){
     //i, j is which index of grid to draw
     int xMin = a*sideLength;
     int yMin = b*sideLength;
     int xMax = ((a+1)*sideLength)-1;
     int yMax = ((b+1)*sideLength)-1;
     int x, y;
-	short int black = 0x0000, white = 0xFFFF;
-    for(x = xMin; x < xMax; x++){
-        for(y = yMin; y < yMax; y++){
+    
+    for(y = yMin; y < yMax; y++){
+        for(x = xMin; x < xMax; x++){
             if(x % 24 == 0 || x % 24 == 23 || y % 24 == 0 || y % 24 == 23){
                 //draw black
-                plot_pixel(x, y, black);
+                plot_pixel(x, y, 0x0000);
             }else{
                 //draw white
-                plot_pixel(x,y,white);
+                plot_pixel(x,y,gridColour);
             }
         }
     }
 }
 
-void drawBackground(){
+void draw_background(){
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
-            draw_grid(i, j);
+            draw_grid(i, j, 0xFFFF);
         }
     }
 }
 
 
-void wallFromLeft(int middleOpening, short int wallColour){
+void wallFromTop(int rowToColour, int middleOpening, short int wallColour){
     //opening is from middleOpening - 1 to middleOpening + 1
-    //using dir, decide where to draw
-    
-}
-
-void wallFromTop(int middleOpening, short int wallColour){
-    //opening is from middleOpening - 1 to middleOpening + 1
-    //using dir, decide where to draw
-    
-}
-
-void wallFromRight(int middleOpening, short int wallColour){
-    //opening is from middleOpening - 1 to middleOpening + 1
-    //using dir, decide where to draw
-    
-}
-
-void wallFromBottom(int middleOpening, short int wallColour){
-    //opening is from middleOpening - 1 to middleOpening + 1
-    //using dir, decide where to draw
-    
-}
-
-void randomBars(){
-    //generate a bar with a random opening, random colour, coming from a random direction
-    int middleOpening = rand()%8+1;
-    int wallColour = colour[rand()%3];
-    int dir = rand()%4;
-    switch(dir){
-        case 0:
-            wallFromLeft(middleOpening, wallColour);
-            break;
-        case 1:
-            wallFromTop(middleOpening, wallColour);
-            break;
-        case 2:
-            wallFromRight(middleOpening, wallColour);
-            break;
-        case 3:
-            wallFromBottom(middleOpening, wallColour);
-            break; 
+    //while(1){
+    for(int row = 0; row < 10; row ++){
+        for(int col = 0; col < 10; col++){
+            if(col == rowToColour){
+                if(row!=middleOpening && row!=middleOpening+1 && row!=middleOpening-1){
+                    draw_grid(row, col, wallColour);
+                }
+            }else draw_grid(row, col, 0xFFFF);//fill the rest in with white
+        }   
     }
+        //wait_for_vsync();
+        //colouredCol++;
+    //}
+}
+
+void wallFromLeft(int rowToColour,int middleOpening, short int wallColour){
+    //opening is from middleOpening - 1 to middleOpening + 1
+   
+    
+}
+
+void wallFromRight(int colToColour,int middleOpening, short int wallColour){
+    //opening is from middleOpening - 1 to middleOpening + 1
     
     
 }
 
-int main(void){
-    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
-    // declare other variables(not shown)
-    // initialize location and direction of rectangles(not shown)
-
-    /* set front pixel buffer to start of FPGA On-chip memory */
-    *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the 
-                                        // back buffer
-    /* now, swap the front/back buffers, to set the front buffer location */
-    wait_for_vsync();
-    /* initialize a pointer to the pixel buffer, used by drawing functions */
-    pixel_buffer_start = *pixel_ctrl_ptr;
-    clear_screen(); // pixel_buffer_start points to the pixel buffer
-    /* set back pixel buffer to start of SDRAM memory */
-    *(pixel_ctrl_ptr + 1) = 0xC0000000;
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
-
-    while (1){   
-        wait_for_vsync(); // swap front and back buffers on VGA vertical sync
-        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
-        drawBackground();
-    }
+void wallFromBottom(int rowToColour,int middleOpening, short int wallColour){
+    //opening is from middleOpening - 1 to middleOpening + 1
+    
+    
 }
-
